1d5041f1f1c2a82af76ac4983a9371a42a3fd9f3
Daiane Geraldino <129515871+DaianeGeraldino@users.noreply.github.com>
2025-09-19 11:44:12 -0300

    feat.: add library discord

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,5 @@
 module.exports = {
     token: 'MTQxODYwNTMzNDY4MzA2MjMyMw.GB39_F.t4i2VfddBC3DPEl7E2NIBECyFEaWvdHWvh2Ji0',
     busApiUrl: 'https://api.onibus.ceturb.es.gov.br/api/BuscaHorarios',
+    prefix: '!',
 }
