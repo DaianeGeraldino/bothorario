1039f08b1a54c6731c23181ce3621413aa7e4536
Daiane Geraldino <daianegeraldino@gmail.com>
2025-09-19 14:43:45 -0300

    fix.: docs config

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: 'MTQxODYwNTMzNDY4MzA2MjMyMw.Gaxdh3.LY9OI7cQTptUBpsodFOdhtdwnJR1Rnf85IgSXE',
+    token: '',
     busApiUrl: 'https://api.onibus.ceturb.es.gov.br/api/BuscaHorarios',
     prefix: '!',
 }
