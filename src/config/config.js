15c4b09c817d00faafea4c8c20df94ec8d2f350f
Daiane Geraldino <daianegeraldino@gmail.com>
2025-09-19 14:41:53 -0300

    feat.: connect bot in discord

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: 'bot_token',
+    token: 'MTQxODYwNTMzNDY4MzA2MjMyMw.Gaxdh3.LY9OI7cQTptUBpsodFOdhtdwnJR1Rnf85IgSXE',
     busApiUrl: 'https://api.onibus.ceturb.es.gov.br/api/BuscaHorarios',
     prefix: '!',
 }
