1aa90cd238a2c90feaa55263f2918007febef92a
Daiane Geraldino <daianegeraldino@gmail.com>
2025-09-19 15:32:20 -0300

    fix.: codding index.js

--- src/index.js
+++ src/index.js
@@ -24,4 +24,6 @@ client.on("messageCreate", async message => {
         const m = await message.channel.send("Ping?");
         m.edit(`Pong! A Latência é ${m.createdTimestamp - message.createdTimestamp}ms. A Latencia da API é ${Math.round(client.ws.ping)}ms`);
     }
+
+    if(comando === "horario")
 });
